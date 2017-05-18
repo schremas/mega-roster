864f679e177c6df53ac71c9ba76ac2fce928deb8
schremas <masonlschreiber@gmail.com>
2017-05-18 10:56:41 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,82 @@
+$(document).foundation()
+
+const megaroster = {
+  students: [],
+
+  init(listSelector) {
+    this.studentList = document.querySelector(listSelector)
+    this.max = 0
+    this.setupEventListeners()  
+  },
+
+  setupEventListeners() {
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent.bind(this))
+  },
+
+  save() {
+    localStorage.setItem('roster', JSON.stringify(this.students))
+  },
+
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    const rosterArray = JSON.parse(rosterString)
+    rosterArray.map((student) => this.addStudent(student))
+  },
+
+  removeStudent(ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
+      }
+    }
+
+    li.remove()
+    this.save()
+  },
+
+  addStudent(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
+    this.students.unshift(student)
+
+    const listItem = this.buildListItem(student)
+    this.prependChild(this.studentList, listItem)
+
+    this.max ++
+    f.reset()
+    this.save()
+  },
+
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
+  },
+
+  buildListItem(student) {
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    this.removeClassName(li, 'template')
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
+    return li
+  },
+
+  removeClassName(el, className){
+    el.className = el.className.replace(className, '').trim()
+  }
+}
+megaroster.init('#studentList')
